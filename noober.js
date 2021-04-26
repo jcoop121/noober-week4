f20ae5b8494bb0c1d7ef77d5da2364b8e7018222
Your Name <you@example.com>
2021-04-25 19:39:58 -0500

    try #2

--- noober.js
+++ noober.js
@@ -13,24 +13,42 @@ window.addEventListener('DOMContentLoaded', async function() {
 
   // with a loop
   // for (declare a variable for counting ("counter variable"); determine the criteria for the number of times to loop; increment the counter)
-  for (let i = 0; i < json.length; i++) {
+  for (let i = 0; i < rideData.length; i++) {
   // console.log(rideData[i])
 
   // Insert HTML into the products element, using the data from each product 
   let rides = rideData[i]
   let ridesElement = document.querySelector(`.rides`)
+
+  let serviceType
+  let borderColor = `border-gray-900`
+  let fontSize = `text-2xl`
+
+  if (rides.purpleRequested == true) {
+    serviceType = `Noober Purple`
+    borderColor = `border-purple-500`
+  } else if (rides.numberOfPassengers > 3) {
+    serviceType = `Noober XL`
+    fontSize = `text-4xl`
+  } 
+  else { 
+    serviceType = `Noober X`
+    }
+
+    // console.log(serviceType)
+
   ridesElement.insertAdjacentHTML(`beforeend`,`
 
-  <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+  <h1 class="inline-block mt-8 px-4 py-2 rounded-xl ${fontSize} bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
   <i class="fas fa-car-side"></i>
-  <span>[Help?]
+  <span>${serviceType}
     </span>
   </h1>
 
-  <div class="border-4 border-gray-900 p-4 my-4 text-left">
+  <div class="border-4 ${borderColor} p-4 my-4 text-left">
     <div class="flex">
       <div class="w-1/2">
-        <h2 class="text-2xl py-1">${rides.passengerDetails.last}, ${rides.passengerDetails.first}</h2>
+        <h2 class="${fontSize} py-1">${rides.passengerDetails.last}, ${rides.passengerDetails.first}</h2>
         <p class="font-bold text-gray-600">${rides.passengerDetails.phoneNumber}</p>
       </div>
       <div class="w-1/2 text-right">
